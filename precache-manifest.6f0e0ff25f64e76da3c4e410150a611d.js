c020208f899139c1c6728798c538b689645d098c
smrutiranjanrana <smrutiranjan.developer@gmail.com>
2019-12-07 17:55:58 +0530

    Updates

---
+++ precache-manifest.6f0e0ff25f64e76da3c4e410150a611d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90b0d4fef08729e6bce0bef0ccde12e",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "07169e5d465b49489a78",
+    "url": "/meme-generator/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "8b5ebd74a51a7fa71ec6",
+    "url": "/meme-generator/static/css/main.bd563752.chunk.css"
+  },
+  {
+    "revision": "07169e5d465b49489a78",
+    "url": "/meme-generator/static/js/2.ae4f7ab4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/meme-generator/static/js/2.ae4f7ab4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b5ebd74a51a7fa71ec6",
+    "url": "/meme-generator/static/js/main.184aa35e.chunk.js"
+  },
+  {
+    "revision": "b70e3563790437b89e78",
+    "url": "/meme-generator/static/js/runtime-main.0aff5e70.js"
+  },
+  {
+    "revision": "3eb51b5fcf620fd70e9a7cdfcc449816",
+    "url": "/meme-generator/static/media/Impact.3eb51b5f.ttf"
+  }
+]);
